f6aacb5a29998608b9dd3cffd4852f8eb326891d
Joshua Näf <naefjo@ethz.ch>
2024-12-27 15:06:46 +0100

    update lazyvim

---
+++ nvim/lua/plugins/fzf.lua
@@ -0,0 +1,14 @@
+return {
+  --  "nvim-telescope/telescope.nvim",
+  --  opts = {
+  --    defaults = {
+  --      path_display = { "filename_first" },
+  --    },
+  --  },
+  "ibhagwan/fzf-lua",
+  opts = {
+    files = {
+      formatter = "path.filename_first",
+    },
+  },
+}
