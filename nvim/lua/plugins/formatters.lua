49732662dd908a629915a30e73f177c8841b8751
Joshua Näf <naefjo@ethz.ch>
2025-03-27 14:23:51 +0100

    add usort to python formatters

--- nvim/lua/plugins/formatters.lua
+++ nvim/lua/plugins/formatters.lua
@@ -2,7 +2,7 @@ return {
   {
     "williamboman/mason.nvim",
     opts = function(_, opts)
-      table.insert(opts.ensure_installed, "isort")
+      table.insert(opts.ensure_installed, "usort")
       table.insert(opts.ensure_installed, "black")
     end,
   },
@@ -12,7 +12,7 @@ return {
     opts = function(_, opts)
       local nls = require("null-ls")
       opts.sources = opts.sources or {}
-      table.insert(opts.sources, nls.builtins.formatting.isort)
+      table.insert(opts.sources, nls.builtins.formatting.usort)
       table.insert(opts.sources, nls.builtins.formatting.black)
     end,
   },
@@ -21,7 +21,7 @@ return {
     optional = true,
     opts = {
       formatters_by_ft = {
-        ["python"] = { "isort", "black" },
+        ["python"] = { "usort", "black" },
       },
     },
   },
