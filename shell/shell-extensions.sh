74b302c59b56e8dafd13cb5c7bd5a5630903c629
Joshua Näf <naefjo@ethz.ch>
2025-09-16 13:48:55 +0200

    make fzf shell integrations "generic"

--- shell/shell-extensions.sh
+++ shell/shell-extensions.sh
@@ -1,12 +1,24 @@
 #!/bin/sh
 
+CURRENT_SHELL=$(basename $SHELL)
+
 export CONFIG_PATH=$HOME/Development/.config
 
 # Custom Keybinds
 alias ll="eza -lah --git"
 
 # Enable fancy prompt
-eval "$(oh-my-posh init $(basename $SHELL) --config ~/Development/.config/omp/theme.omp.toml)"
+eval "$(oh-my-posh init $CURRENT_SHELL --config ~/Development/.config/omp/theme.omp.toml)"
+
+# Bash integration
+if [ -n "$BASH_VERSION" ]; then
+  eval "$(fzf --bash)"
+fi
+
+# Zsh integration
+if [ -n "$ZSH_VERSION" ]; then
+  source <(fzf --zsh)
+fi
 
 # Install custom scripts
 export PATH=$CONFIG_PATH/scripts:$PATH
