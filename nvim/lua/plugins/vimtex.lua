2c4f4169915c261d39f3f84c73fbd131fbeb90b9
Joshua Näf <naefjo@ethz.ch>
2025-10-28 16:26:17 +0100

    [vimtex] add backup viewer if texshop not available

--- nvim/lua/plugins/vimtex.lua
+++ nvim/lua/plugins/vimtex.lua
@@ -3,7 +3,11 @@ return {
     "lervag/vimtex",
     lazy = false, -- we don't want to lazy load VimTeX
     init = function()
-      vim.g.vimtex_view_method = "texshop"
+      if vim.fn.executable("texshop") == 1 then
+        vim.g.vimtex_view_method = "texshop"
+      else
+        vim.g.vimtex_view_method = "general"
+      end
       vim.g.vimtex_compiler_latexmk = {
         out_dir = "build",
       }
